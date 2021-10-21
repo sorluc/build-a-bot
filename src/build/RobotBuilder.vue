0cb5dc6cab0831d663039c222eee3ec2884da599
Stephane Orluc <stephane.orluc@forgerock.com>
2021-10-21 19:04:39 +0200

    using :class biding

--- src/build/RobotBuilder.vue
+++ src/build/RobotBuilder.vue
@@ -2,7 +2,7 @@
   <div class="content">
     <button class="add-to-cart" @click="addToCart()">Add to Cart</button>
     <div class="top-row">
-      <div class="top part" :style="[headBorderStyle]">
+      <div class="top part" :class="{'sale-border':selectedRobot.head.onSale}">
         <div class="robot-name">
           {{selectedRobot.head.title}}
           <span v-show="selectedRobot.head.onSale"
@@ -271,5 +271,8 @@ td, th {
 }
 .cost {
   text-align: right;
+}
+.sale-border {
+  border: 3px solid red;
 }
 </style>
